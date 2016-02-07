3597773fee1741861bd2ae31985ed50ccc24b335
Nicolas Vivet <nicolas@nzx.io>
2016-02-07 17:30:14 +0100

    follow multiple collectors

--- follow_announces.py
+++ follow_announces.py
@@ -9,25 +9,30 @@ from tabi.rib import Radix
 logger = logging.getLogger(__name__)
 
 
+COLLECTORS=["rrc18", "rrc19", "rrc20", "rrc21"]
+
+
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description="get a feed ofannounces from a BGP collector")
-    parser.add_argument("collector")
+    parser.add_argument("collector", nargs="+", default=COLLECTORS)
     parser.add_argument("--offset", type=int)
     parser.add_argument("--prefixes-file")
+    parser.add_argument("--our-servers", default=",".join(["comet-17-08.sdsc.edu:9092","comet-17-03.sdsc.edu:9092", "comet-17-22.sdsc.edu:9092", "comet-17-24.sdsc.edu:9092"]))
 
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.INFO)
 
-    consumer = KafkaConsumer("rib-{}".format(args.collector),
-                             bootstrap_servers=["comet-17-08.sdsc.edu:9092"],
+    topics = ["rib-{}".format(c) for c in args.collector]
+
+    consumer = KafkaConsumer(*topics,
+                             bootstrap_servers=args.our_servers.split(","),
                              group_id="follower")
 
     if args.offset is not None:
-        topics = [("rib-{}".format(args.collector), 0, args.offset)]
-        consumer.set_topic_partitions(*topics)
+        consumer.set_topic_partitions({(t, 0): args.offset for t in topics})
 
     # setup filters
     filters = []
